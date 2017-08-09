2c057ea2c5dc0457de070c5628981ee8fa3d70a1
Eric Griffis <dedbox@gmail.com>
2017-08-09 15:34:49 -0700

    drop unused ship var from flight-mode

--- flight-mode.gd
+++ flight-mode.gd
@@ -1,17 +1,14 @@
 extends Node2D
 
 var ship
-var stars
 
 func _ready():
-	stars = get_node("Stars")
 	ship = get_node("Ship")
 	set_process(true)
 	set_process_input(true)
 
 func _process(delta):
 	ship.travel(delta)
-	set_pos(Vector2(960, 540) - ship.get_pos())
 
 func _input(event):
 	if event.is_action_pressed("ui_left"):
